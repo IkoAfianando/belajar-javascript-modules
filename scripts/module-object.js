543ad7d0092b77d2674f2e4595059e650334835d
IkoAfianando <ikoafianando123@gmail.com>
2021-12-13 07:07:52 +0700

    Push

---
+++ scripts/module-object.js
@@ -0,0 +1,15 @@
+export const firstName = "Iko";
+export const lastName = "Afianando";
+export const fullName = "Iko Afianando";
+
+export function sayHello() {
+    console.log(`Hello from Iko`);
+}
+
+export class Person {
+    constructor() {
+        this.name = "Iko"
+    }
+}
+
+
